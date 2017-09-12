56eaa02afb576a4c6ae840d521f32f0e28855c5d
Samuel Lampa <samuel.lampa@gmail.com>
2017-09-12 21:38:40 +0200

    Get rid of code we don't use

--- main.go
+++ main.go
@@ -3,13 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/knakk/rdf"
-	"github.com/knakk/sparql"
 	"io"
 	"log"
 	"net/http"
 	"strings"
-	"time"
 )
 
 func main() {
@@ -25,12 +22,12 @@ func main() {
 		log.Fatal("No namespace provided. Use the -h flag to view options")
 	}
 
-	// Some status info
+	// Print some output to the console
 	fmt.Println("Connecting to SPARQL Endpoint with URL:", *endpoint)
 	fmt.Println("Starting to serve at: " + *host + ":" + *port + " ...")
 
 	// Start handling requests
-	uriResHandler := &UriResolverHandlerSimple{*namespace, *endpoint}
+	uriResHandler := &UriResolverHandler{*namespace, *endpoint}
 	http.Handle("/", uriResHandler)
 	err := http.ListenAndServe(*host+":"+*port, nil)
 	if err != nil {
@@ -50,68 +47,6 @@ type UriResolverHandler struct {
 func (h *UriResolverHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	uri := h.Namespace + r.URL.Path[1:]
 
-	// Connect to SPARQL Endpoint
-	repo, err := sparql.NewRepo(h.SparqlEndpointUrl,
-		sparql.DigestAuth("", ""),
-		sparql.Timeout(time.Millisecond*1500),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Run SPARQL query
-	results, err := repo.Query("DESCRIBE <" + uri + ">")
-	if err != nil {
-		log.Println(err)
-	}
-
-	for _, sol := range results.Solutions() {
-		subj, err := rdf.NewIRI(sol["subject"].String())
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		pred, err := rdf.NewIRI(sol["predicate"].String())
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var obj rdf.Object
-		switch sol["object"].Type() {
-		case rdf.TermBlank:
-			obj, err = rdf.NewBlank(sol["object"].String())
-			if err != nil {
-				log.Fatal(err)
-			}
-		case rdf.TermIRI:
-			obj, err = rdf.NewIRI(sol["object"].String())
-			if err != nil {
-				log.Fatal(err)
-			}
-		case rdf.TermLiteral:
-			obj, err = rdf.NewLiteral(sol["object"].String())
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-
-		triple := rdf.Triple{subj, pred, obj}
-		fmt.Fprint(w, triple.Serialize(rdf.Turtle))
-	}
-}
-
-// UriResolverHandlerSimple works similarly to UriResoverHandler, but does
-// aquire the data from the SPARQL endpoint in a simpler way: It just receives
-// the raw RDF/XML aquired when doing a DESCRIBE SPARQL query, and forwards it
-// to the user, instead of doing any parsing of the results.
-type UriResolverHandlerSimple struct {
-	Namespace         string
-	SparqlEndpointUrl string
-}
-
-func (h *UriResolverHandlerSimple) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	uri := h.Namespace + r.URL.Path[1:]
-
 	reader := strings.NewReader(`query=DESCRIBE <` + uri + `>`)
 	request, err := http.NewRequest("POST", h.SparqlEndpointUrl, reader)
 	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
@@ -120,11 +55,11 @@ func (h *UriResolverHandlerSimple) ServeHTTP(w http.ResponseWriter, r *http.Requ
 	}
 
 	client := &http.Client{}
-	resp, err := client.Do(request)
+	response, err := client.Do(request)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Just forward the raw RDF/XML from Blazegraph
-	io.Copy(w, resp.Body)
+	io.Copy(w, response.Body)
 }
