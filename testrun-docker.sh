3d45f8ad2f75d6bcda12f29f4df03c8de3253ca7
Samuel Lampa <samuel.lampa@gmail.com>
2017-09-18 13:33:57 +0200

    Add testrun-docker.sh script

---
+++ testrun-docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "After the service started below, open a browser and surf in on http://localhost:8080/entity/Q3 ..."
+docker run \
+    -it \
+    -p 8080:8080 \
+    --name urisolve-test \
+    --hostname urisolve \
+    --rm farmbio/urisolve \
+    urisolve \
+    -endpoint https://query.wikidata.org/sparql \
+    -urihost http://www.wikidata.org \
+    -host urisolve \
+    -port 8080
