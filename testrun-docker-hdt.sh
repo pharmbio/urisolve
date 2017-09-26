d3d437be6fba207d5f1f31e10a850f777319613e
Samuel Lampa <samuel.lampa@gmail.com>
2017-09-26 23:08:54 +0200

    Fix testrun-docker-hdt.sh

---
+++ testrun-docker-hdt.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+export HDTFILE=/usr/go/src/github.com/pharmbio/urisolve/example_data.hdt
+echo "After the service started below, open a browser and surf in on http://localhost:8080/entity/Q3 ..."
+docker run \
+    -it \
+    -p 8080:8080 \
+    --name urisolve-test-hdt \
+    --hostname urisolve \
+    --rm \
+    farmbio/urisolve
+
+# Full command, if you want to customize some parameters to the urisolve binary
+#docker run \
+#    -it \
+#    -p 8080:8080 \
+#    --name urisolve-test-hdt \
+#    --hostname urisolve \
+#    --rm farmbio/urisolve \
+#    urisolve \
+#    -srctype hdt \
+#    -hdtfile $HDTFILE \
+#    -urihost http://rdf.pharmb.io \
+#    -host urisolve \
+#    -port 8080
